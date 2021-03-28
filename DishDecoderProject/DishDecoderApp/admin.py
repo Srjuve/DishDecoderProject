a030a8b11c5e6d4fb5c1658834af0aff03a4fb60
Adrià Juvé <estudisjuveadria@gmail.com>
2021-03-28 22:05:50 +0200

    Models with Constraints added to admin interface

--- DishDecoderProject/DishDecoderApp/admin.py
+++ DishDecoderProject/DishDecoderApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Recipes,BasicProducts,Nutrients,Ratings,Recipe_Product,Product_Nutrients
+ #Register your models here.
+admin.site.register(Recipes)
+admin.site.register(BasicProducts)
+admin.site.register(Nutrients)
+admin.site.register(Ratings)
+admin.site.register(Recipe_Product)
+admin.site.register(Product_Nutrients)
--- DishDecoderProject/DishDecoderApp/models.py
+++ DishDecoderProject/DishDecoderApp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+#from django.conf import settings
 # Create your models here.
+class Recipes(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    steps = models.CharField(max_length=2000)
+
+class BasicProducts(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+
+class Nutrients(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+
+class Ratings(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_autor = models.ForeignKey(User,on_delete=models.CASCADE)
+    id_recipe = models.ForeignKey(Recipes,on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0,validators=[MaxValueValidator(10),MinValueValidator(0)])
+    desc = models.CharField(max_length=1000)
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['id_autor','id_recipe'],name="unique_key_pairs_autor_recipe"),]
+
+class Recipe_Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_recipe = models.ForeignKey(Recipes,on_delete=models.CASCADE)
+    id_product = models.ForeignKey(BasicProducts,on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=6,decimal_places=3,default=0,validators=[MinValueValidator(0)])
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['id_recipe','id_product'],name="unique_key_pairs_recipe_product"),]
+
+
+class Product_Nutrients(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_product = models.ForeignKey(BasicProducts,on_delete=models.CASCADE)
+    id_nutrient = models.ForeignKey(Nutrients,on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=6,decimal_places=3,default=0,validators=[MinValueValidator(0)])
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['id_product','id_nutrient'],name="unique_key_pairs_product_nutrient"),]
+    
+
