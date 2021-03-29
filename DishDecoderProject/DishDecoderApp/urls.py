73293b29703e9744adf9c805653469022e4be288
Adrià Juvé <estudisjuveadria@gmail.com>
2021-03-29 18:02:23 +0200

    Created view and added forms,links and prepared rendering of template

--- DishDecoderProject/DishDecoderApp/urls.py
+++ DishDecoderProject/DishDecoderApp/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 from DishDecoderApp import views
 urlpatterns = [
-    path('',views.main_url)
+    path('',views.main_url),
 ]
--- DishDecoderProject/DishDecoderApp/views.py
+++ DishDecoderProject/DishDecoderApp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseForbidden,HttpResponseBadRequest,HttpResponseNotAllowed,HttpResponseNotFound
 from .models import *
-from .forms import *
+from .forms import Main_page_form
 # Create your views here.
 
 def main_url(req):
-    return HttpResponse('PlaceHolder')
+    template_name="DishDecoderApp/main_url.html"
+    template_data = {}
+    if req.method == 'GET':
+        form = Main_page_form()
+        template_data['form']=form
+        template_data['login_button_link']="/login/"
+        template_data['search_form_link']="/search/"
+        return render(req, template_name,template_data)
+    return HttpResponseBadRequest
