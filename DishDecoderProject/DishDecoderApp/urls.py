9c5e8eaa44db2e9ffa1626dcb632a499a3991183
GuillemCamats <64152349+GuillemCamats@users.noreply.github.com>
2021-03-31 17:40:20 +0200

    urls i navbar

--- DishDecoderProject/DishDecoderApp/urls.py
+++ DishDecoderProject/DishDecoderApp/urls.py
@@ -17,14 +17,14 @@ from django.contrib import admin
 from django.urls import path, re_path
 from DishDecoderApp import views
 urlpatterns = [
-    path('',views.main_url),
-    path('login/',views.login_page_url),
-    path('register/',views.register_page_url),
-    path('logout/',views.logout_url),
-    path('recipes/',views.list_recipes_url),
+    path('',views.main_url, name = "home"),
+    path('login/',views.login_page_url, name="login"),
+    path('register/',views.register_page_url, name="register"),
+    path('logout/',views.logout_url, name="logout"),
+    path('recipes/',views.list_recipes_url, name="recipes"),
     path('recipe/<int:recipeid>', views.recipe_profile_url, name="recipe"),
-    path('basicproducts/',views.list_basicproducts_url),
+    path('basicproducts/',views.list_basicproducts_url, name="basicproducts"),
     path('basicproduct/<int:basicproductid>',views.basicproduct_profile_url, name="basicproduct"),
-    path('nutrients/',views.list_nutrients_url),
+    path('nutrients/',views.list_nutrients_url, name="nutrients"),
     path('nutrient/<int:nutrientid>',views.nutrient_profile_url, name="nutrient")
 ]
