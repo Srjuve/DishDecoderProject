588a4d06a5bb58f369fdca8fd94f52ed6623a7d4
santo0 <lemarti3472@gmail.com>
2021-04-09 15:39:46 +0200

    added timeout and workers gunicorn. fixed lochost

--- DishDecoderProject/settings/production_docker.py
+++ DishDecoderProject/settings/production_docker.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 # Application definition
 
--- build_script.sh
+++ build_script.sh
@@ -2,4 +2,4 @@
 python manage.py migrate
 
 #python manage.py runserver 0.0.0.0:8000
-gunicorn -b 0.0.0.0:8000 DishDecoderProject.wsgi:application
+gunicorn -b 0.0.0.0:8000 DishDecoderProject.wsgi:application --timeout 200 --workers 3
