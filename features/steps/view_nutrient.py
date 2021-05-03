c46338b3e61203aa3d3d5814d1a5be95ebce1adb
rogerLarriba <roglarbau@gmail.com>
2021-05-03 19:06:10 +0200

    View Nutrient Behaviour Tests

--- features/steps/view_nutrient.py
+++ features/steps/view_nutrient.py
@@ -2,6 +2,7 @@ from behave import *
 from DishDecoderApp.models import Nutrients
 
 use_step_matcher("parse")
+#Test 1
 @given(u'Exists a nutrient id "{id}"')
 def step_impl(context,id):
     Nutrients.objects.create(id=id, name='Nutrient1', desc="Nutrient made for the behaviour test")
@@ -15,3 +16,32 @@ def step_impl(context,id):
 def step_impl(context):
     elems = context.browser.find_by_tag('h1')
     assert elems[0].text == "Nutrient1"
+
+#Test 2
+@given(u'Exists a nutrient id "{id}" without description')
+def step_impl(context,id):
+    Nutrients.objects.create(id=id, name='Nutrient2')
+
+@when(u'I search the nutrient id "{id}" without description')
+def step_impl(context,id):
+    from DishDecoderApp.models import Nutrients
+    context.browser.visit(context.get_url("/nutrient/"+id))
+
+@then(u'I\'m viewing the details page for the nutrient without description')
+def step_impl(context):
+    #assert context.browser.is_text_present == "Nutrient2"
+    elems = context.browser.find_by_tag('h1')
+    assert elems[0].text == "Nutrient2"
+    assert context.browser.is_text_present('No Data Found')
+
+#Test 3
+@when(u'I try to search the nutrient id "{id}" which doesn\'t exist')
+def step_impl(context,id):
+    context.browser.visit(context.get_url("/nutrient/"+id))
+
+
+@then(u'I\'m expecting to receive an error')
+def step_impl(context):   
+    assert context.browser.is_text_present('Error 404')
+    assert context.browser.is_text_present('The page you are trying to reach does not exist')
+
--- features/view_nutrient.feature
+++ features/view_nutrient.feature
@@ -3,7 +3,17 @@ Feature: View Nutrient
     As a user,
     I want to view the information related to a nutrient.
 
-Scenario: View Recipe
+Scenario: View Nutrient
     Given Exists a nutrient id "1"
     When I search the nutrient id "1"
     Then I'm viewing the details page for the nutrient id
+
+Scenario: View Nutrient without Description
+    Given Exists a nutrient id "2" without description
+    When I search the nutrient id "2" without description
+    Then I'm viewing the details page for the nutrient without description
+
+Scenario: Trying to view a nonexistent Nutrient
+    When I try to search the nutrient id "3" which doesn't exist
+    Then I'm expecting to receive an error
+
