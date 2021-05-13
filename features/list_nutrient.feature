1c9bdd63bc9bc82f2c53166f2faeee8dea50827b
GuillemCamats <guillemcamats@gmail.com>
2021-05-13 18:32:35 +0200

    test nutrients actualized

--- features/list_nutrient.feature
+++ features/list_nutrient.feature
@@ -4,10 +4,10 @@ Feature: View list of Nutrients
     I want to see a list of nutrients.
 
 Scenario: User search nutrients
-    Given A nutrient with id "1" and a nutrient with id "2"
-    When I click nutrients and i search nutrient and i click search
-    Then I'm seeing a list
+    Given A nutrient with id "1" and name "nutrient1" a nutrient with id "2" with name "nutrient2"
+    When I click nutrients and i search "nutrient" and i click search
+    Then I'm seeing the nutrients "nutrient1" and "nutrient2"
 
 Scenario: User search for an inexistent recipe
-    When I search for a nutrient that not exist
+    When I search for "nutrient"
     Then It appears error message "No Data Found"
--- features/steps/list_nutrients.py
+++ features/steps/list_nutrients.py
@@ -5,31 +5,31 @@ from utils import toggle_down_navbar
 
 use_step_matcher("parse")
 #test1
-@given(u'A nutrient with id "{id}" and a nutrient with id "{id2}"')
-def step_impl(context,id,id2):
-    Nutrients.objects.create(id=id,name="nutrient1",desc="test2")
-    Nutrients.objects.create(id=id2,name="nutrient2",desc="test1")
+@given(u'A nutrient with id "{id}" and name "{name1}" a nutrient with id "{id2}" with name "{name2}"')
+def step_impl(context,id,name1,id2,name2):
+    Nutrients.objects.create(id=id,name=name1,desc="test2")
+    Nutrients.objects.create(id=id2,name=name2,desc="test1")
 
-@when(u'I click nutrients and i search nutrient and i click search')
-def step_impl(context):
+@when(u'I click nutrients and i search "{word}" and i click search')
+def step_impl(context,word):
     context.browser.visit(context.get_url("/"))
     form = context.browser.find_by_tag('form')
     form.find_by_id('id_request_objective_2').first.click()
-    context.browser.fill('item_name', "nutrient")
+    context.browser.fill('item_name', word)
     form.find_by_css('input[name="submit"]').first.click()
 
-@then(u'I\'m seeing a list')
-def step_impl(context):
-    assert context.browser.is_text_present('nutrient1')
-    assert context.browser.is_text_present('nutrient2')
+@then(u'I\'m seeing the nutrients "{name1}" and "{name2}"')
+def step_impl(context,name1,name2):
+    assert context.browser.is_text_present(name1)
+    assert context.browser.is_text_present(name2)
 
 #test2
-@when(u'I search for a nutrient that not exist')
-def step_impl(context):
+@when(u'I search for "{name}"')
+def step_impl(context,name):
     context.browser.visit(context.get_url("/"))
     form = context.browser.find_by_tag('form')
     form.find_by_id('id_request_objective_2').first.click()
-    context.browser.fill('item_name', "recipe")
+    context.browser.fill('item_name', name)
     form.find_by_css('input[name="submit"]').first.click()
 
 @then(u'I\'m seeing an error')
