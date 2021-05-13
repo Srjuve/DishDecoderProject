ddd3e0c31447a9922a2e7e91ab0aa76676716953
santo0 <lemarti3472@gmail.com>
2021-05-13 18:34:42 +0200

    exists a recipe implemented

--- features/steps/delete_recipe.py
+++ features/steps/delete_recipe.py
@@ -4,14 +4,17 @@ from utils import toggle_down_navbar
 
 use_step_matcher("parse")
 
-@given(u'Exists a recipe "{recipe_title}" created by "{recipe_author}"')
-def step_impl(context, recipe_title, recipe_author):
-    recipe = Recipes.objects.create(name=recipe_title, author=recipe_author, steps="")
+@given(u'Exists a recipe "{recipe_title}" created by "{author_name}"')
+def step_impl(context, recipe_title, author_name):
+    from DishDecoderApp.models import Recipes
+    from django.contrib.auth.models import User
+    author = User.objects.filter(username=author_name).first()
+    recipe = Recipes.objects.create(name=recipe_title, author=author, steps="")
 
 
 @when(u'I click on erase recipe button')
 def step_impl(context):
-    
+    raise NotImplementedError(u'STEP: I click on erase recipe button')    
 
 
 @when(u'I select "soup" for deleting purposes')
