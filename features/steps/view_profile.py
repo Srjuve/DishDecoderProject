fe16cb173c13e8c34867f1d46341b496797913e6
rogerLarriba <roglarbau@gmail.com>
2021-05-15 13:45:50 +0200

    Changes on view_profile behaviour

--- features/steps/view_profile.py
+++ features/steps/view_profile.py
@@ -1,14 +1,10 @@
 from behave import *
 from django.contrib.auth.models import User
-from DishDecoderApp.models import Recipes
+from DishDecoderApp.models import Ratings, Recipes
 
 use_step_matcher("parse")
-#Test 1
-@given(u'Exists a User "{user}" but it\'s not logged')
-def step_impl(context,user):
-    createdUser = User.objects.create_user(username=user, email='testbehave@potatoe.com', password='Exemple123')
-    
-@when(u'I\'ll try to see my profile')
+#Test 1    
+@when(u'I\'ll try to see my profile through the url')
 def step_impl(context):
     context.browser.visit(context.get_url("/profile/"))
 
@@ -20,24 +16,52 @@ def step_impl(context):
 
 
 #Test 2 
-@given(u'I, as "{user}", am logged in the system')
-def step_impl(context,user):
-    createdUser = User.objects.create_user(username=user, email='testbehave@potatoe.com', password='Exemple123')
-    #Em falta loggejar
+@then(u'I\'ll see the user\'s profile\'s information.')
+def step_impl(context):
+    assert context.browser.is_text_present('User Profile')
+    foundh1 = context.browser.find_by_tag('h1')
+    assert foundh1[1]
+
+
+#Test3
+@given (u'I made the recipe "{recipe}"')
+def step_impl(context,user,recipe,password):
+    createdUser = User.objects.create_user(username=user, email='testbehave@potatoe.com', password=password)
+    Recipes.objects.create(name=recipe,author=createdUser,steps="Podria tenir tomata")
     context.browser.visit(context.get_url("/profile/"))
     form = context.browser.find_by_tag('form').first
     context.browser.fill('username', user)
-    context.browser.fill('password', 'Exemple123')
+    context.browser.fill('password', password)
     form.find_by_id('submit-login').first.click()
 
 
-@when(u'I\'m going to my profile')
-def step_impl(context):
-    context.browser.visit(context.get_url("/profile/"))
-
-
-@then(u'I\'ll bee seeing my profile\'s information')
-def step_impl(context):
+@then (u'I, as user "UsuariTestBehave", will also see some details about his/her recipe "{recipe}"')
+def step_impl(context,user,recipe):
     assert context.browser.is_text_present('User Profile')
-    assert context.browser.is_text_present('Change password')
-    assert context.browser.is_text_present('Change email')
+    assert context.browser.is_text_present(recipe)
+    
+
+
+#Test4
+@given (u'The user "{user}" with the password "{password}" is logged and he made a rating "{rating}" on the recipe "{recipe}" made by the other user "{chefUser}" with the password "{passwordChef}"')
+def step_impl(context,user, password,rating,recipe,chefUser, passwordChef):
+    createdChef = User.objects.create_user(username=chefUser, email='testchef@potatoe.com', password=passwordChef)
+    createdRecipe = Recipes.objects.create(name=recipe,author=createdChef,steps="Podria tenir tomata")
+    createdReviewer = User.objects.create_user(username=user, email='testReviewer@potatoe.com', password=password)
+    Ratings.objects.create(id_autor = createdReviewer,id_recipe = createdRecipe,rating = 5,desc = rating)
+
+    context.browser.visit(context.get_url("/profile/"))
+    form = context.browser.find_by_tag('form').first
+    context.browser.fill('username', user)
+    context.browser.fill('password', password)
+    form.find_by_id('submit-login').first.click()
+
+@then (u'The user "{user}" will also see some details about his/her rating "{rating}" on the recipe "{recipe}" made by the other user "{chefUser}"')
+def step_impl(context,user, rating, recipe,chefUser):
+    assert context.browser.is_text_present('User Profile')
+    assert context.browser.is_text_present(user)
+    assert context.browser.is_text_present(rating)
+    assert context.browser.is_text_present(recipe)
+    assert context.browser.is_text_present(chefUser)
+
+
--- features/view_profile.feature
+++ features/view_profile.feature
@@ -3,13 +3,28 @@ Feature: View Profile
     As a user,
     I want to view the information related to my profile.
 
-Scenario: Trying to see my profile without being logged ###REVISAR###
-    Given Exists a User "UsuariTestBehaveNoLoggejat" but it's not logged
-    When I'll try to see my profile
-    Then I'll receive an error
+Background:
+    Given Exists a user "UsuariTestBehave" with password "Exemple123"
+
+
+Scenario: Trying to see my profile without being logged
+    When I'll try to see my profile through the url
+    Then I'll be redirected to the login page
 
 Scenario: Viewing my profile 
-    Given I, as "UsuariTestBehave", am logged in the system
-    When I'm going to my profile
-    Then I'll bee seeing my profile's information
+    Given I click on the login button
+    And I login as user "UsuariTestBehave" with password "Exemple123"
+    When I click on profile button
+    Then I'll see the user's profile's information.
 
+Scenario: Viewing the profile of a user who has a recipe
+    Given I click on the login button
+    And I login as user "UsuariTestBehave" with password "Exemple123"
+    And I made the recipe "TestRecipeYummy"
+    When I click on profile button
+    Then I, as user "UsuariTestBehave", will also see some details about his/her recipe "TestRecipeYummy"
+
+Scenario: Viewing the profile of a user who made a rating
+    Given The user "UserReviewer" with the password "elCritico123" is logged and he made a rating "NiceRate" on the recipe "RatedRecipe" made by the other user "ElChefNuestro" with the password "CuinerCasaNostra123"
+    When I click on profile button
+    Then The user "UserReviewer" will also see some details about his/her rating "NiceRate" on the recipe "RatedRecipe" made by the other user "ElChefNuestro"
