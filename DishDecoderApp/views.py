49509b62ef89d62dc6375dd8e272209c36975cb0
santo0 <lemarti3472@gmail.com>
2021-05-21 13:48:18 +0200

    New possible solution.

--- DishDecoderApp/views.py
+++ DishDecoderApp/views.py
@@ -1,4 +1,5 @@
-from DishDecoderProject.settings import API_KEY
+from django.conf import settings
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse,HttpResponseForbidden,HttpResponseBadRequest,HttpResponseNotAllowed,HttpResponseNotFound
@@ -30,7 +31,7 @@ class main_url(View):
         template_data['form']=self.form
         template_data['aform'] = self.aform
         template_data['title_page']='Dish Decoder'
-        template_data['api_key'] = API_KEY
+        template_data['api_key'] = settings.API_KEY
         return render(req, self.template_name,template_data)
 
     def post(self,req):
